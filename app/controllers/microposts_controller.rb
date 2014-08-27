0dce5bd1657e6c11fa70f3182734160b3690108a
Richmond Watkins <richmondwatkins@mac.com>
2014-08-27 10:43:45 -0500

    Add user micropost

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,37 @@
+class MicropostsController < ApplicationController
+  before_action :signed_in_user
+  before_action :correct_user, only: [:destroy]
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @micropost = Micropost.find_by(id: params[:id])
+      redirect_to root_url unless current_user?(@micropost.user)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   has_secure_password
 
+  has_many :microposts, dependent: :destroy
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -15,10 +17,15 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
       self.remember_token = User.digest(User.new_remember_token)
     end
-    
+
 end
